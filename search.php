e207f8944e40e830580be036704c53f41a994027
Sanjida_Samanta <101191638+Hima0X2@users.noreply.github.com>
2024-05-30 20:24:59 +0600

    Add files via upload

---
+++ search.php
@@ -0,0 +1,32 @@
+<?php
+$search_value = $_POST["search"];
+$conn = mysqli_connect('localhost:3307', 'root', '', 'sign') or die("Connection failed");
+
+$sql = "SELECT * FROM userinfo WHERE username LIKE '%{$search_value}%'";
+$result = mysqli_query($conn, $sql) or die("SQL Query failed");
+
+if (mysqli_num_rows($result) > 0) {
+    $output = '<table border="1" cellspacing="0" cellpadding="10">
+    <tr>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Edit</th>
+        <th>Delete</th>
+    </tr>';
+    while ($row = mysqli_fetch_assoc($result)) {
+        $output .= '<tr>
+        <td>' . $row["username"] . '</td>
+        <td>' . $row["email"] . '</td>
+        <td>' . $row["Upassword"] . '</td>
+        <td><button class="update-btn" data-ename="' . $row["username"] . '">Edit</button></td>
+        <td><button class="delete-btn" data-username="' . $row["username"] . '">Delete</button></td>
+        </tr>';
+    }
+    $output .= '</table>';
+    mysqli_close($conn);
+    echo $output;
+} else {
+    echo "<h2>No record found.</h2>";
+}
+?>
